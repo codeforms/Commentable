2196b16465455259e3f8fb4c01a01a1960617758
codeforms <fatih.kececi@yahoo.co.uk>
2020-05-11 22:12:35 +0300

    Updated migration file

--- migrations/2020_04_09_161800_create_comments_table.php
+++ migrations/2020_04_09_161800_create_comments_table.php
@@ -17,11 +17,6 @@ class CreateCommentsTable extends Migration
             $table->integer('user_id')->unsigned();
             $table->integer('parent_id')->unsigned()->nullable();
             $table->timestamps();
-
-            $table->foreign('user_id')
-                ->references('id')
-                ->on('users')
-                ->onDelete('cascade');
         });
     }
 
